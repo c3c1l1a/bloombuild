e6bd58cc203aa6e850ef61e6ccc5bf59cc1021dc
c3c1l1a <cesswangumukima@gmail.com>
2023-02-17 20:42:35 +0800

    Remove loader for now

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,21 +1,7 @@
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-
-export const loader = async () => {
-  return json({
-    blocks: [
-      {type: 'text'},
-      {type: 'text'},
-      {type: 'text'},
-    ]
-  })
-}
-
 export default function Index() {
-  const { blocks } = useLoaderData();
-  console.log(blocks);
   return (
-    <main >   
+    <main >  
+    main 
     </main>
   );
 }
