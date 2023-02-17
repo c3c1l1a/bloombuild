1d1ef3427788bce98b4d42032a4f1edbef159a0f
c3c1l1a <cesswangumukima@gmail.com>
2023-02-17 20:26:53 +0800

    Define 3 text blocks

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,38 +1,21 @@
-import { Link } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
-import { useOptionalUser } from "~/utils";
-
-
+export const loader = async () => {
+  return json({
+    blocks: [
+      {type: 'text'},
+      {type: 'text'},
+      {type: 'text'},
+    ]
+  })
+}
 
 export default function Index() {
-  const user = useOptionalUser();
+  const { blocks } = useLoaderData();
+  console.log(blocks);
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">        
-      <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-        {user ? (
-          <Link
-            to="/notes"
-            className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-          >
-            View Notes for {user.email}
-          </Link>
-        ) : (
-          <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-            <Link
-              to="/join"
-              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
-            >
-              Sign up
-            </Link>
-            <Link
-              to="/login"
-              className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-            >
-              Log In
-            </Link>
-          </div>
-        )}
-      </div>
+    <main >   
     </main>
   );
 }
