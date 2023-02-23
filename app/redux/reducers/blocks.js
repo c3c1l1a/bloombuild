b40d27830d8fbd7e57e230a3ba75d36be591b8ee
c3c1l1a <cesswangumukima@gmail.com>
2023-02-23 21:21:41 +0800

    Function to remove item from state

--- app/redux/reducers/blocks.js
+++ app/redux/reducers/blocks.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { current } from '@reduxjs/toolkit'
 
 const initialState = {
+	id: 'main-1',
 	type: 'Main',
 	children: [
 		{ 
@@ -24,12 +26,26 @@ const initialState = {
 	]
 }
 
+function removeItem (state, draggedItem){
+	if (state.children) {
+		const filteredChild = state.children.filter((child) => child.id === draggedItem.id);
+		if (filteredChild.length === 0){
+			state.children.map((child) => {
+				return removeItem(child, draggedItem)
+			});
+		} else {
+			const filteredChildren = state.children.filter((child) => child.id !== draggedItem.id)
+			state.children = filteredChildren;
+		}
+	};
+}
+
 const blockSlice = createSlice({
 	name: 'block',
 	initialState,
 	reducers: {
 		drop(state, action){
-			console.log(action.payload);
+			removeItem(state, action.payload.draggedItem);
 			return state;
 		}
 	},
